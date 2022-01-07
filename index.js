4d5fe740af3c7b1464a37d50f284a89697325a0c
alejillo2501 <alejandro.minus@gmail.com>
2022-01-07 14:18:22 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const cors = require('cors');
+const services = require('./service');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+const allowedOrigins = [
+    'capacitor://localhost',
+    'capacitor-electron://*',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100',
+    'http://localhost:9876'
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    },
+  };
+
+
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/today', cors(corsOptions), async function (req, res) {
+    const info = await services.bitCointToday();
+
+    let body = {
+        status: 500,
+        data: [],
+        message: 'Error.',
+        date: ''
+    }
+
+    if(info.data.data == undefined){
+        body = {
+            status: 404,
+            data: [],
+            message: 'Servicio sin funcionar.',
+            date: info.date
+        }
+    }else{
+        body = {
+            status: 200,
+            data: info.data.data,
+            message: 'Resultado cargado correctamente.',
+            date: info.date
+        }
+    }
+    
+  res.send(body);
+});
+app.get('/lastFifteenDays', cors(corsOptions), async function (req, res) {
+    const info = await services.bitCointLast15Days();
+    let body = {
+        data: [],
+        message: 'Error',
+        status: 500
+    }
+    
+    if(info.length > 0){
+        body = {
+            data: info,
+            message: 'Resultados cargados últimos 15 días',
+            status: 200
+        }
+    }else{
+        body = {
+            data: [],
+            message: 'No se pudo cargar los resultados de los últimos 15 días',
+            status: 404
+        }
+    }
+    
+  res.send(body);
+});
+
+
+app.post('/bydate', cors(corsOptions), async function (req, res) {
+    const info = await services.bitCointByDate(req.body.item);
+
+    let body = {
+        status: 200,
+        data: info,
+        message: 'Resultados cargados para la fecha: '+req.body.item,
+        date: req.body.item
+    }
+    
+  res.send(body);
+});
+
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
---
+++ service.js
@@ -0,0 +1,94 @@
+const fetch = require('cross-fetch');
+const d = new Date();
+const month = d.getMonth()+1;
+const year = d.getFullYear();
+const endpoint = 'https://api.coinbase.com/v2/prices/BTC-USD/spot';
+
+module.exports = {
+    bitCointToday : async() => {
+        
+        const response = await fetch(endpoint, {
+            method: 'GET'
+        }).then((resp) => resp.json());
+        
+        var body = {
+            date: d,
+            data: response
+        }
+        return body;
+    },
+    bitCointLast15Days : async() =>{
+        var cant = 15;
+        var dayLast = d.getDate();
+        var monthLast = month;
+        var yearLast = year;
+
+        var array = [];
+
+        while (cant > 0) {
+            dayLast--;
+
+            if(dayLast == 0){
+                monthLast--;
+                if(monthLast == 0){
+                    monthLast = 12;
+                    yearLast--;
+                }
+                dayLast = daysMonth(monthLast, yearLast);
+            }
+
+            var dateLast = yearLast+'-'+monthLast+'-'+dayLast;
+
+            const info = await fetch(endpoint+'?date='+dateLast, {
+                method: 'GET'
+            }).then((resp) => resp.json());
+            
+            if(info.data != undefined){
+                var dLast = new Date(yearLast, monthLast-1, dayLast);
+
+                var body = {
+                    date:dLast,
+                    info: info.data,
+                    item: dateLast
+                }
+
+                array.push(body);
+            }
+
+            cant--;
+        }
+
+        return array;
+
+    },
+    bitCointByDate: async(item) => {
+
+        const USD = await fetch('https://api.coinbase.com/v2/prices/BTC-USD/spot?date='+item, {
+            method: 'GET'
+        }).then((resp) => resp.json());
+
+        const EUR = await fetch('https://api.coinbase.com/v2/prices/BTC-EUR/spot?date='+item, {
+            method: 'GET'
+        }).then((resp) => resp.json());
+
+        const COP = await fetch('https://api.coinbase.com/v2/prices/BTC-COP/spot?date='+item, {
+            method: 'GET'
+        }).then((resp) => resp.json());
+
+        var spItem = item.split('-');
+        var dItem = new Date(spItem[0],spItem[1], spItem[2]);
+        
+        var body = {
+            item: dItem,
+            COP: COP,
+            EUR: EUR,
+            USD: USD
+        }
+
+        return body;
+    }
+}
+
+function daysMonth(montCal, yearCal) {
+	return new Date(yearCal, montCal, 0).getDate();
+}
